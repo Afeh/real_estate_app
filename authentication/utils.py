98302a48d5fc6501eff5c0b5020ab98dfb2aa561
Afebu Balogun <balogunafebu22@gmail.com>
2024-10-23 21:05:37 +0100

    Remove env

--- authentication/utils.py
+++ authentication/utils.py
@@ -3,10 +3,11 @@ from django.core.mail import send_mail
 from django.utils import timezone
 from datetime import timedelta
 from .models import OTP
+from django.conf import settings
 
 def generate_otp(email):
-	# otp_code = str(random.randint(100000, 999999))
-	otp_code = "123456" #Change during production
+	otp_code = str(random.randint(100000, 999999))
+	# otp_code = "123456" #Change during production
 	expiry_time = timezone.now() + timedelta(minutes=10)
 
 	OTP.objects.create(email=email, otp=otp_code, expires_at=expiry_time)
@@ -14,6 +15,6 @@ def generate_otp(email):
 	send_mail(
 		subject="OTP Code from Real Estate Team",
 		message=f'Your OTP code is {otp_code}. Please do not share with anybody. Your code is valid for 10 minutes.',
-		from_email='realestate@example.com',
+		from_email=settings.EMAIL_HOST_USER,
 		recipient_list=[email],
 	)
--- real_estate/settings.py
+++ real_estate/settings.py
@@ -173,4 +173,20 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# EMAIL_BACKEND = env("EMAIL_BACKEND")
+# EMAIL_HOST = env("EMAIL_HOST")
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+# EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+# DEFAULT_FROM_EMAIL = env("DEFAULT_FROM_EMAIL")
+
+EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_PORT=587
+EMAIL_USE_TLS=True
+EMAIL_HOST_USER='balogunafebu22@gmail.com'
+EMAIL_HOST_PASSWORD='pchymjzfuouwtoou'
+DEFAULT_FROM_EMAIL='balogunafebu22@gmail.com'
